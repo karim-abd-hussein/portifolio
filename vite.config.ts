97a593a6b8c947dc5669007784355b9f1d60d050
Karim Hussein <abobalal.karim@gmail.com>
2025-01-29 19:48:58 +0300

    version 1 basic strcture and ready to deploy on vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/portifolio/'
+  // base:'/portifolio/'
 })
+// "predeploy": "npm run build",
+// "deploy": "gh-pages -d dist",
